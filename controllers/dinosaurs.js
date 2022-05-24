afbb582643a256b699b1ad1f58cf23bff7c38e3d
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-24 06:36:10 -0600

    Add routes to update/destory dinos & edit view

--- controllers/dinosaurs.js
+++ controllers/dinosaurs.js
@@ -39,4 +39,56 @@ router.get("/:id", (req, res) => {
   res.render("dinosaurs/show", { dino })
 })
 
+// GET /dinosaurs/edit/:id -- a view of a form to edit a specific dino @ :id
+router.get("/edit/:id", (req, res) => {
+  // get the dino data and render the edit form
+  const dinosaurs = fs.readFileSync("./dinosaurs.json")
+  const dinoData = JSON.parse(dinosaurs)
+
+  // render edit form
+  res.render("dinosaurs/edit.ejs", {
+    dinoId: req.params.id,
+    dino: dinoData[req.params.id],
+  })
+})
+
+// PUT /dinosaurs/:id -- update a dino @ :id in the database
+router.put("/:id", (req, res) => {
+  // get the dinos from the dino json
+  const dinosaurs = fs.readFileSync("./dinosaurs.json")
+  const dinoData = JSON.parse(dinosaurs)
+  console.log(req.params.id, req.body)
+
+  // update the dino based on the req.params.id and the req.body
+  // req.params = which dino
+  // req.body = dino data to updated
+  dinoData[req.params.id].name = req.body.name
+  dinoData[req.params.id].type = req.body.type
+
+  // write the json file
+  fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinoData))
+
+  // redirect to some place that has interesting data
+  res.redirect("/dinosaurs") // go back to all dinos
+  // res.redirect(`/dinosaurs/${req.params.id}`) // just see this one dino
+  // res.send(`update a dino @ ${req.params.id}`)
+})
+
+// DELETE /dinosaurs/:id -- DESTROY a dino @ :id
+router.delete("/:id", (req, res) => {
+  // get the dinos from the dino json
+  const dinosuars = fs.readFileSync("./dinosaurs.json")
+  const dinoData = JSON.parse(dinosuars)
+
+  // splice dino out of the array (index fro mthe req.params)
+  // Array.splice(starting index to remove, how many elements to remove)
+  dinoData.splice(req.params.id, 1)
+
+  // save the dino json
+  fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinoData))
+  // redirect to see all dinos
+  // res.send(`DESTROY a poor dino @ ${req.params.id}`)
+  res.redirect("/dinosaurs")
+})
+
 module.exports = router
